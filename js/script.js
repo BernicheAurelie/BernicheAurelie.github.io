0ba87bdf9b93aa957c4a9cf5cde01182904dbf68
BernicheAurelie <berniche.aurelie@orange.fr>
2024-01-22 09:54:05 +0100

    try auto google translate with default language browser

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+const lang = document.documentElement.getAttribute('lang') || 'en';
+function Init() {
+  new google.translate.TranslateElement({ pageLanguage: `${lang}` });
+}
+
+const usrlang = navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2);
+if (usrlang !== `${lang}`) {
+  document.cookie = `googtrans=/${lang}/${usrlang}; Expires=Session; SameSite=None; Secure`;
+  const google = document.createElement('script');
+  google.type = 'text/javascript';
+  google.src = 'https://translate.google.com/translate_a/element.js?cb=Init';
+  document.body.append(google);
+  setTimeout(removePreloader, 1200);
+} else removePreloader();
+
+function removePreloader() {
+  const preloader = document.querySelector('#preloader');
+  if (preloader) preloader.remove();
+}
