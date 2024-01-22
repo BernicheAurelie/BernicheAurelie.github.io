8af96a2f390fd9b92d591b3998fc928346a254d9
BernicheAurelie <berniche.aurelie@orange.fr>
2024-01-22 11:13:38 +0100

    remove auto translate

--- js/script.js
+++ js/script.js
@@ -3,29 +3,30 @@ const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-const lang = document.documentElement.getAttribute('lang') || 'en';
-console.log("constante lang initial: ", lang);
-function Init() {
-    console.log("constante lang in init: ", lang);
-  new google.translate.TranslateElement({ pageLanguage: `${lang}` });
-  console.log("constante lang post translate: ", lang)
-}
+// auto google translate
+// const lang = document.documentElement.getAttribute('lang') || 'en';
+// console.log("constante lang initial: ", lang);
+// function Init() {
+//     console.log("constante lang in init: ", lang);
+//   new google.translate.TranslateElement({ pageLanguage: `${lang}` });
+//   console.log("constante lang post translate: ", lang)
+// }
 
-const usrlang = navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2);
-if (usrlang !== `${lang}`) {
-    console.log("constante usrlang/lang: ", usrlang, " / ", lang);
-    console.log("document.cookie initial: ", document.cookie)
-  document.cookie = `googtrans=/${lang}/${usrlang}; Expires=Session; SameSite=None; Secure`;
-  console.log("document.cookie googtrans: ", document.cookie)
-  const google = document.createElement('script');
-  console.log("const google: ", google)
-  google.type = 'text/javascript';
-  google.src = 'https://translate.google.com/translate_a/element.js?cb=Init';
-  document.body.append(google);
+// const usrlang = navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2);
+// if (usrlang !== `${lang}`) {
+//     console.log("constante usrlang/lang: ", usrlang, " / ", lang);
+//     console.log("document.cookie initial: ", document.cookie)
+//   document.cookie = `googtrans=/${lang}/${usrlang}; Expires=Session; SameSite=None; Secure`;
+//   console.log("document.cookie googtrans: ", document.cookie)
+//   const google = document.createElement('script');
+//   console.log("const google: ", google)
+//   google.type = 'text/javascript';
+//   google.src = 'https://translate.google.com/translate_a/element.js?cb=Init';
+//   document.body.append(google);
 //   setTimeout(removePreloader, 1200);
-} else removePreloader();
+// } else removePreloader();
 
-function removePreloader() {
-  const preloader = document.querySelector('#preloader');
-  if (preloader) preloader.remove();
-}
+// function removePreloader() {
+//   const preloader = document.querySelector('#preloader');
+//   if (preloader) preloader.remove();
+// }
